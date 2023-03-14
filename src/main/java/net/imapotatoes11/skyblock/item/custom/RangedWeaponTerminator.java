14a857439304f2eb1b0e7b9b52eba18bfa18309d
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2023-03-13 22:55:32 -0400

    minor changes (reverted terminator code cuz cleaning it up broke it??)

--- src/main/java/net/imapotatoes11/skyblock/item/custom/RangedWeaponTerminator.java
+++ src/main/java/net/imapotatoes11/skyblock/item/custom/RangedWeaponTerminator.java
@@ -1,5 +1,6 @@
 package net.imapotatoes11.skyblock.item.custom;
 
+import net.imapotatoes11.skyblock.item.custom.util.Util;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
@@ -15,15 +16,37 @@ public class RangedWeaponTerminator extends Item {
     }
 
     private static ArrowEntity setupArrow(World world, PlayerEntity user, int direction){
+        // direction:
+        // 0 = none
+        // -1 = left
+        // 1 = right
+
         ArrowEntity arrow = new ArrowEntity(EntityType.ARROW, world);
 //        arrow.teleport(
 //                user.getX(),
 //                user.getY()+1.6,
 //                user.getZ()
 //        );
-        _ = (user.isSneaking()) ? arrow.teleport(user.getX(),user.getY()+1.3, user.getZ()):
-                arrow.teleport(user.getX(),user.getY()+1.6,user.getZ());
-        return null;
+        if (user.isSneaking()) {
+            arrow.teleport(user.getX(), user.getY() + 1.3, user.getZ());
+        } else {
+            arrow.teleport(user.getX(), user.getY() + 1.6, user.getZ());
+        }
+
+        //these two are not needed, but values can be changed to spice things up a bit
+        arrow.setNoGravity(false);
+        arrow.setNoClip(false);
+
+        arrow.setVelocity(Util.VEC_0);
+        switch (direction){
+            case 1:
+                arrow.addVelocity(user.getRotationVector().multiply(5).rotateY((float) Math.toRadians(-15)));
+            case 0:
+                arrow.addVelocity(user.getRotationVector().multiply(5));
+            case -1:
+                arrow.addVelocity(user.getRotationVector().multiply(5).rotateY((float) Math.toRadians(15)));
+        }
+        return arrow;
     }
 
     @Override
@@ -67,6 +90,11 @@ public class RangedWeaponTerminator extends Item {
         rightArrow.setVelocity(0,0,0);
         // make arrow go around 30 degrees to the right of the player's rotation vector
         rightArrow.addVelocity(user.getRotationVector().multiply(5).rotateY((float) Math.toRadians(-15)));
+
+        // this breaks it??? what??
+//        world.spawnEntity(setupArrow(world, user, 0));
+//        world.spawnEntity(setupArrow(world, user, -1));
+//        world.spawnEntity(setupArrow(world, user, 1));
 
         world.spawnEntity(middleArrow);
         world.spawnEntity(leftArrow);
--- src/main/java/net/imapotatoes11/skyblock/item/custom/util/Util.java
+++ src/main/java/net/imapotatoes11/skyblock/item/custom/util/Util.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 
 public class Util {
+    public static final Vec3d VEC_0 = new Vec3d(0,0,0);
     public static void teleport(PlayerEntity user, int distance){
         Vec3d unitVector = user.getRotationVector();
         unitVector = unitVector.normalize().multiply(distance);
