c1a29f255ceb7312c2b39bd6fc1ed886277fef36
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2023-05-26 17:30:55 -0400

    minor changes

--- src/main/java/net/imapotatoes11/skyblock/item/custom/ToolGrapplingHook.java
+++ src/main/java/net/imapotatoes11/skyblock/item/custom/ToolGrapplingHook.java
@@ -1,19 +1,12 @@
 package net.imapotatoes11.skyblock.item.custom;
 
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.FishingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
-import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
 
 public class ToolGrapplingHook extends FishingRodItem {
     public ToolGrapplingHook(Item.Settings settings){
